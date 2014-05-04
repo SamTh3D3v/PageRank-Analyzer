478b447af3f976c30b60922a4895f082d9ae2c31
aymendaoudi <aymendaoudi@outlook.com>
2014-05-04 14:12:05 +0100

    Commit WGM2

---
+++ WebGraphMaker/Model/Link.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace WebGraphMaker.Model
+{
+    public class Link
+    {
+        [XmlElement("From")] 
+        public short TailPageId { get; set; }
+
+        [XmlElement("To")] 
+        public short HeadPageId { get; set; }
+    }
+}
---
+++ WebGraphMaker/Model/Page.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WebGraphMaker.Model
+{
+    public class Page
+    {   
+        [XmlIgnore]
+        public Uri Url { get; set; }
+        
+        
+        [XmlAttribute("uri")]
+        [Browsable(false), EditorBrowsable(EditorBrowsableState.Never)]
+        public string UriString
+        {
+            get { return Url == null ? null : Url.ToString(); }
+            set { Url = (value == null) ? null : new Uri(value); }
+        }
+
+
+        [XmlElement("Id")] 
+        public short Id { get; set; }
+
+        [XmlElement("Description")] 
+        public String Description { get; set; }
+    }
+}

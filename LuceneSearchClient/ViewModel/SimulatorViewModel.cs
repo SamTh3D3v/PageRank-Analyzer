fe4dd93b1382a0ce80d286ec396d147c1ffd3838
SamTheDev <abdelkhalekdev@Gmail.Com>
2014-05-10 17:59:10 -0400

    Generate matrix Commit

--- LuceneSearchClient/ViewModel/SimulatorViewModel.cs
+++ LuceneSearchClient/ViewModel/SimulatorViewModel.cs
@@ -1,4 +1,6 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace LuceneSearchClient.ViewModel
 {
@@ -8,10 +10,18 @@ namespace LuceneSearchClient.ViewModel
         #region Consts
         public const string MatrixSizePropertyName = "MatrixSize";
         public const string TransitionMatrixPropertyName = "TransitionMatrix";
+        public const string InitialPageRankPropertyName = "InitialPageRank";
+        public const string DampingFactorPropertyName = "DampingFactor";
+        public const string TransportationValuePropertyName = "TransportationValue";
+        public const string NumberIterationsPropertyName = "NumberIterations";
         #endregion
         #region Fields
         private int _matrixSize;
         private short[,] _transitionMatrix;
+        private float _dampingFactor;
+        private float _transportationValue;
+        private float _initialPageRank;
+        private int _numberIterations;
         #endregion
         #region Properties
         public int MatrixSize
@@ -28,7 +38,9 @@ namespace LuceneSearchClient.ViewModel
                     return;
                 }                
                 _matrixSize = value;
-                TransitionMatrix = new short[_matrixSize, _matrixSize];                
+                TransitionMatrix = new short[_matrixSize, _matrixSize];
+                TransportationValue = 1f/_matrixSize;
+                InitialPageRank = 1f / _matrixSize;
                 RaisePropertyChanged(MatrixSizePropertyName);
             }
         }
@@ -45,20 +57,128 @@ namespace LuceneSearchClient.ViewModel
                 {
                     return;
                 }
-
-                RaisePropertyChanging(TransitionMatrixPropertyName);
+                
                 _transitionMatrix = value;
                 RaisePropertyChanged(TransitionMatrixPropertyName);
+            }
+        }
+        public float DampingFactor
+        {
+            get
+            {
+                return _dampingFactor;
+            }
+
+            set
+            {
+                if (Equals(_dampingFactor, value))
+                {
+                    return;
+                }
+                
+                _dampingFactor = value;
+                RaisePropertyChanged(DampingFactorPropertyName);
+            }
+        }        
+        public float TransportationValue
+        {
+            get
+            {
+                return _transportationValue;
+            }
+
+            set
+            {
+                if (Equals(_transportationValue, value))
+                {
+                    return;
+                }
+                
+                _transportationValue = value;
+                RaisePropertyChanged(TransportationValuePropertyName);
+            }
+        }        
+        public float InitialPageRank
+        {
+            get
+            {
+                return _initialPageRank;
+            }
+
+            set
+            {
+                if (Equals(_initialPageRank, value))
+                {
+                    return;
+                }                
+                _initialPageRank = value;
+                RaisePropertyChanged(InitialPageRankPropertyName);
+            }
+        }           
+        public int NumberIterations
+        {
+            get
+            {
+                return _numberIterations;
+            }
+
+            set
+            {
+                if (_numberIterations == value)
+                {
+                    return;
+                }
+                
+                _numberIterations = value;
+                RaisePropertyChanged(NumberIterationsPropertyName);
             }
         }
         #endregion
         #region Ctors and Methods
         public SimulatorViewModel()
         {
+            DampingFactor = 0.85f;
+            NumberIterations = 100;
+            
+
         }
         #endregion
         #region Commands
-
+        private RelayCommand _generateMatrixCommand;   
+        public RelayCommand GenerateMatrixCommand
+        {
+            get
+            {
+                return _generateMatrixCommand
+                    ?? (_generateMatrixCommand = new RelayCommand(
+                                          () =>
+                                          {
+                                              var rand=new Random();
+                                              for (int i = 0; i < MatrixSize; i++)
+                                              {
+                                                  for (int j = 0; j < MatrixSize; j++)
+                                                  {
+                                                      TransitionMatrix[i, j] = (short) rand.Next(2);
+                                                  }
+                                                  
+                                              }
+                                              RaisePropertyChanged(TransitionMatrixPropertyName);
+                                          }));
+            }
+        }
+        private RelayCommand _resetMatrixCommand;
+        public RelayCommand ResetMatrixCommand 
+        {
+            get
+            {
+                return _resetMatrixCommand
+                    ?? (_resetMatrixCommand = new RelayCommand(
+                                          () =>
+                                          {
+                                              TransitionMatrix = new short[_matrixSize, _matrixSize];
+                                          }));
+            }
+        }
         #endregion
        
     }

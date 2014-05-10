f99e5622deb8ddeecadf1117f31aaeb21fc56b8a
SamTheDev <abdelkhalekdev@Gmail.Com>
2014-05-10 15:03:49 -0400

    Transition Matrix Generation Commit

---
+++ LuceneSearchClient/Converters/MatrixToDataViewConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace LuceneSearchClient.Converters
+{
+    public class MatrixToDataViewConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var array = value as short[,];
+            if (array == null) return null;
+            //rows
+            if (array.GetLength(0) == 0) return null;            
+            if (array.GetLength(1) == 0) return null;
+            var dataTable = new DataTable();
+            for (var column = 0; column < array.GetLength(1); column++)
+            {
+                dataTable.Columns.Add(new DataColumn(column.ToString(CultureInfo.InvariantCulture)));
+            }            
+            for (var row = 0; row < array.GetLength(0); row++)
+            {
+                var newRow = dataTable.NewRow();
+                for (var column = 0; column < array.GetLength(1); column++)
+                {
+                    newRow[column] = array[row, column];
+                }
+                dataTable.Rows.Add(newRow);
+            }
+            return dataTable.DefaultView;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- LuceneSearchClient/ViewModel/SimulatorViewModel.cs
+++ LuceneSearchClient/ViewModel/SimulatorViewModel.cs
@@ -5,9 +5,61 @@ namespace LuceneSearchClient.ViewModel
 
     public class SimulatorViewModel : ViewModelBase
     {
-     
+        #region Consts
+        public const string MatrixSizePropertyName = "MatrixSize";
+        public const string TransitionMatrixPropertyName = "TransitionMatrix";
+        #endregion
+        #region Fields
+        private int _matrixSize;
+        private short[,] _transitionMatrix;
+        #endregion
+        #region Properties
+        public int MatrixSize
+        {
+            get
+            {
+                return _matrixSize;
+            }
+
+            set
+            {
+                if (_matrixSize == value)
+                {
+                    return;
+                }                
+                _matrixSize = value;
+                TransitionMatrix = new short[_matrixSize, _matrixSize];                
+                RaisePropertyChanged(MatrixSizePropertyName);
+            }
+        }
+        public short[,] TransitionMatrix
+        {
+            get
+            {
+                return _transitionMatrix;
+            }
+
+            set
+            {
+                if (_transitionMatrix == value)
+                {
+                    return;
+                }
+
+                RaisePropertyChanging(TransitionMatrixPropertyName);
+                _transitionMatrix = value;
+                RaisePropertyChanged(TransitionMatrixPropertyName);
+            }
+        }
+        #endregion
+        #region Ctors and Methods
         public SimulatorViewModel()
         {
         }
+        #endregion
+        #region Commands
+
+        #endregion
+       
     }
 }

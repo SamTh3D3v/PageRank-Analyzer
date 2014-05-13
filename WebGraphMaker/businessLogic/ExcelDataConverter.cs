ff7e41a084b3af113a62cd3c5d549606c2d85c8f
aymendaoudi <aymendaoudi@outlook.com>
2014-05-13 04:03:33 +0100

    ExcelDataConverter XmlDocumentation + ExcelDataConverter (Outputs XML created file names)

--- WebGraphMaker/businessLogic/ExcelDataConverter.cs
+++ WebGraphMaker/businessLogic/ExcelDataConverter.cs
@@ -16,116 +16,147 @@ namespace WebGraphMaker.businessLogic
 {
     public class ExcelDataConverter
     {
-        
+        #region Constructors
+
+        /// <summary>
+        /// Initilaize new instance of the ExcelDataConverter Class, using a specified range
+        /// </summary>
+        /// <param name="range">The suggested range from which to convert WebGraph data</param>
         public ExcelDataConverter(Range range)
         {
             _worksheetRange = range;
         }
 
+        #endregion
+
         #region Methods
-                private void GeneratePagesList()
+
+        /// <summary>
+        /// Generates The list of pages from the current ExcelDataReader instance's Range object
+        /// </summary>
+        private void GeneratePagesList()
+        {
+            _pages = new List<Model.Page>();
+
+            int rowCount = _worksheetRange.Rows.Count;
+            int colCount = _worksheetRange.Columns.Count;
+
+            ulong idCount = 0;
+            for (int i = 1; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
                 {
-                     _pages = new List<Model.Page>();
+                    //Thx god Uri has the == operator
 
-                     int rowCount = _worksheetRange.Rows.Count;
-                     int colCount = _worksheetRange.Columns.Count;
-
-                     ulong idCount = 0;
-                     for (int i = 1; i <= rowCount; i++)
-                     {
-                         for (int j = 1; j <= colCount; j++)
-                         {
-                             //Thx god Uri has the == operator
-
-                             if (_pages.Count == 0)
-                             {
-                                 Debug.WriteLine(idCount);
-                                 _pages.Add(new Model.Page()
-                                 {
-
-                                     Url = new Uri(_worksheetRange.Cells[i, j].Value2.ToString(), UriKind.RelativeOrAbsolute),
-                                     Id = idCount
-                                 });
-                                 idCount++;
-                             }
-                             else
-                             {
-                                 var uri = new Uri(_worksheetRange.Cells[i, j].Value2.ToString(), UriKind.RelativeOrAbsolute);
-                                 if (!_pages.Contains(_pages.Find(p => p.Url == uri)))
-                                 {
-                                     Debug.WriteLine(idCount);
-                                     _pages.Add(new Model.Page()
-                                     {
-                                         Url = new Uri(_worksheetRange.Cells[i, j].Value2.ToString(), UriKind.RelativeOrAbsolute),
-                                         Id = idCount
-                                     });
-                                     idCount++;
-                                 }
-                             }
-                         }
-                     }
-
-                }
-                private void GenerateLinksList()
-                {
-                    _links = new List<Model.Link>();
-
-                    int rowCount = _worksheetRange.Rows.Count;
-                    
-                    for (int i = 1; i <= rowCount; i++)
+                    if (_pages.Count == 0)
                     {
-                        var tailUri = new Uri(_worksheetRange.Cells[i, 1].Value2.ToString(), UriKind.RelativeOrAbsolute);
-                        var headUri = new Uri(_worksheetRange.Cells[i, 2].Value2.ToString(), UriKind.RelativeOrAbsolute);
-                        ulong tailId = _pages.Find(p => p.Url == tailUri).Id;
-                        ulong headId = _pages.Find(p => p.Url == headUri).Id;
-                        _links.Add(new Link()
+                        Debug.WriteLine(idCount);
+                        _pages.Add(new Model.Page()
                         {
-                            TailPageId = tailId,
-                            HeadPageId = headId
+
+                            Url = new Uri(_worksheetRange.Cells[i, j].Value2.ToString(), UriKind.RelativeOrAbsolute),
+                            Id = idCount
                         });
-                    } 
-                }
-                private void PersistGraphEntitiesToXml(GraphEntities grapheEntity)
-                {
-                    var saveFileDialog = new SaveFileDialog
+                        idCount++;
+                    }
+                    else
                     {
-                        Filter = "txt files (*.xml)|*.xml",
-                        FilterIndex = 2,
-                        RestoreDirectory = true
-                    };
-
-                    
-                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
-                    {
-                        using (TextWriter writer = new StreamWriter(saveFileDialog.FileName))
+                        var uri = new Uri(_worksheetRange.Cells[i, j].Value2.ToString(), UriKind.RelativeOrAbsolute);
+                        if (!_pages.Contains(_pages.Find(p => p.Url == uri)))
                         {
-                            if (grapheEntity == GraphEntities.Pages)
+                            Debug.WriteLine(idCount);
+                            _pages.Add(new Model.Page()
                             {
-                                var pagesSerializer = new XmlSerializer(typeof(List<Model.Page>));
-                                pagesSerializer.Serialize(writer, _pages);
-                            }
-                            else if (grapheEntity == GraphEntities.Links)
-                            {
-                                var linksSerializer = new XmlSerializer(typeof(List<Link>));
-                                linksSerializer.Serialize(writer, _links);      
-                            }
+                                Url = new Uri(_worksheetRange.Cells[i, j].Value2.ToString(), UriKind.RelativeOrAbsolute),
+                                Id = idCount
+                            });
+                            idCount++;
                         }
                     }
                 }
-                public void ConvertExelData()
-                {
-                    GeneratePagesList();
-                    GenerateLinksList();
+            }
 
-                    PersistGraphEntitiesToXml(GraphEntities.Pages);
-                    PersistGraphEntitiesToXml(GraphEntities.Links);
+        }
+        /// <summary>
+        /// Generates The list of links from the current ExcelDataReader instance's Range object
+        /// </summary>
+        private void GenerateLinksList()
+        {
+            _links = new List<Model.Link>();
+
+            int rowCount = _worksheetRange.Rows.Count;
+
+            for (int i = 1; i <= rowCount; i++)
+            {
+                var tailUri = new Uri(_worksheetRange.Cells[i, 1].Value2.ToString(), UriKind.RelativeOrAbsolute);
+                var headUri = new Uri(_worksheetRange.Cells[i, 2].Value2.ToString(), UriKind.RelativeOrAbsolute);
+                ulong tailId = _pages.Find(p => p.Url == tailUri).Id;
+                ulong headId = _pages.Find(p => p.Url == headUri).Id;
+                _links.Add(new Link()
+                {
+                    TailPageId = tailId,
+                    HeadPageId = headId
+                });
+            }
+        }
+
+        /// <summary>
+        /// Writes Pages or Links lists into XML files, ouptuts the file name of the created XML file
+        /// </summary>
+        /// <param name="grapheEntity">The suggested entity to persist can be Page or Link</param>
+        /// <param name="fileName">The full name of the generated XML file</param>
+        private void PersistGraphEntitiesToXml(GraphEntities grapheEntity, out string fileName)
+        {
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "txt files (*.xml)|*.xml",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName = saveFileDialog.FileName;
+                using (TextWriter writer = new StreamWriter(fileName))
+                {
+                    if (grapheEntity == GraphEntities.Pages)
+                    {
+                        var pagesSerializer = new XmlSerializer(typeof(List<Model.Page>));
+                        pagesSerializer.Serialize(writer, _pages);
+                    }
+                    else if (grapheEntity == GraphEntities.Links)
+                    {
+                        var linksSerializer = new XmlSerializer(typeof(List<Link>));
+                        linksSerializer.Serialize(writer, _links);
+                    }
                 }
+            }
+            else
+            {
+                fileName = null;
+            }
+        }
+
+        /// <summary>
+        /// Converts Excel data into a WebGraph by generating XML files, outputs the names of the both created XML files 
+        /// </summary>
+        /// <param name="pagesFileName">The full file name of the created pages XML file</param>
+        /// <param name="linksFileName">The full file name of the created links XML file</param>
+        public void ConvertExelData(out string pagesFileName, out string linksFileName)
+        {
+            GeneratePagesList();
+            GenerateLinksList();
+
+            PersistGraphEntitiesToXml(GraphEntities.Pages, out pagesFileName);
+            PersistGraphEntitiesToXml(GraphEntities.Links, out linksFileName);
+        }
         #endregion
 
         #region Fields
 
-            private List<Model.Page> _pages;
-            private List<Model.Link> _links;
+        private List<Model.Page> _pages;
+        private List<Model.Link> _links;
 
         private readonly Range _worksheetRange;
 
@@ -134,6 +165,6 @@ namespace WebGraphMaker.businessLogic
 
     public enum GraphEntities
     {
-        Pages,Links
+        Pages, Links
     }
 }

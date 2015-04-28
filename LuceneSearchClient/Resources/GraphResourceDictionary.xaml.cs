3a120ff201c6d45ba5169ef1bf7501e273fa16c8
samthedev <abdelkhalekdev@gmail.com>
2015-04-28 15:28:52 +0100

    Add the missing GraphResourceDictionary Code behind

---
+++ LuceneSearchClient/Resources/GraphResourceDictionary.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LuceneSearchClient.Resources
+{
+    public partial class GraphResourceDictionary : ResourceDictionary
+    {
+        public GraphResourceDictionary()
+        {
+            this.InitializeComponent();
+        }
+
+        private void RemoveNode_ClickEvent(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void StartArc_ClickEvent(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EndArc_ClickEvent(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
